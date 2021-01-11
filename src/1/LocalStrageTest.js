8f92e66cd6fd9308228be329ba0436673714660b
ytyaru <yttry0@gmail.com>
2021-01-11 19:43:29 +0900

    add Keys, show()

--- src/1/LocalStrageTest.js
+++ src/1/LocalStrageTest.js
@@ -5,6 +5,18 @@ export default class LocalStrageTest {
     }
     get Key() { return this.KEY; }
     get Value() { return localStorage.getItem(this.KEY); }
+    get Keys() {
+        let keys = [];
+        for (let i=0; i<localStorage.length; i++) { keys.push(localStorage.key(i)); }
+        return keys;
+//        for (let i=0; i<localStorage.length; i++) { yield keys.push(localStorage.key(i); }
+    }
+    show() {
+        console.log('Show all keys.');
+        for (let key of this.Keys) {
+            console.log(`  ${key}=${localStorage.getItem(key)}`);
+        }
+    }
     load() {
         console.log(`Loaded !! ${this.KEY}=${this.Value}`);
     }
--- src/1/main.js
+++ src/1/main.js
@@ -1,6 +1,7 @@
 import LocalStrageTest from './LocalStrageTest.js';
 window.addEventListener('load', (event) => {
     let test = new LocalStrageTest();
+    test.show();
     test.load();
     test.save();
     test.remove();
